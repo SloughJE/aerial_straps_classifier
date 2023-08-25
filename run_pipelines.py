d9eefbd8641c5c0dce309af079552aa528d7dc15
SloughJE <SloughJE@gmail.com>
2023-08-25 09:18:07 -0400

    to match filepaths

--- run_pipelines.py
+++ run_pipelines.py
@@ -1,9 +1,8 @@
 import sys
 import argparse
 import yaml
-from src.data.load_data import load_raw_data
-from src.labeling.label import run_labeling
-from src.labeling.video_processing import reduce_video_size
+from src.data.label import run_labeling
+from src.data.video_processing import reduce_video_size
 from src.features.make_features import extract_landmarks_and_features
 
 if __name__ == "__main__":
@@ -11,20 +10,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--load_data",
-        help="load raw data",
-        action="store_true"
-    )
-     
-    parser.add_argument(
-        "--label_data",
-        help="label raw data",
+        "--reduce_quality",
+        help="reduce video quality for quicker labeling",
         action="store_true"
     )
 
     parser.add_argument(
-        "--reduce_quality",
-        help="reduce video quality for quicker labeling",
+        "--label_data",
+        help="label raw data",
         action="store_true"
     )
 
@@ -41,12 +34,6 @@ if __name__ == "__main__":
     else:
         with open("params.yaml") as f:
             params = yaml.safe_load(f)
-
-        if args.load_data:
-            load_raw_data(
-                'data/raw/test.csv',
-                'data/interim/processed.csv'
-            )
 
         if args.reduce_quality:
             reduce_video_size(
