db3c88886467bd59739e49e5b9e95b5c3f3fd7ab
SloughJE <SloughJE@gmail.com>
2023-09-26 11:24:09 -0400

    fixing paths n stuff

--- api/main.py
+++ api/main.py
@@ -19,42 +19,46 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 
 # Constants
-UPLOAD_DIR = "api/uploaded_images"
-MODEL_PATH = "models/dev/xgb/xgb_model.joblib"
-LABEL_ENCODER_PATH = "models/dev/xgb/label_encoder.pkl"
-Path(UPLOAD_DIR).mkdir(parents=True, exist_ok=True)
 base_directory = Path(__file__).parent
-templates = Jinja2Templates(directory="api/templates")
 
-app.mount("/uploaded_images", StaticFiles(directory=base_directory / "uploaded_images"), name="uploaded_images")
+# Constants
+UPLOAD_DIR = base_directory / "uploaded_images"
+MODEL_PATH = base_directory.parent / "models" / "dev" / "xgb" / "xgb_model.joblib"
+LABEL_ENCODER_PATH = base_directory.parent / "models" / "dev" / "xgb" / "label_encoder.pkl"
 
+# Create the directory if it doesn't exist
+UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
+
+# Define the templates directory
+TEMPLATES_DIR = base_directory / "templates"
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
+
+# Mount static files using the absolute path
+app.mount("/uploaded_images", StaticFiles(directory=UPLOAD_DIR), name="uploaded_images")
 
 @app.get("/", response_model=None)
 async def serve_page():
     """Serves the main page."""
     return templates.TemplateResponse("index.html", {"request": {}})
 
-
-def save_uploaded_file(file: UploadFile, new_filename: str = "temp.jpg") -> str:
+def save_uploaded_file(file: UploadFile, new_filename: str = "temp.jpg") -> Path:
     """Saves the uploaded file and returns its path."""
-    input_path = os.path.join(UPLOAD_DIR, new_filename)
-    with open(input_path, "wb") as buffer:
+    input_path = UPLOAD_DIR / new_filename
+    with input_path.open("wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     return input_path
 
-
-def extract_image_features(input_path: str) -> Tuple[object, str, str]:
+def extract_image_features(input_path: Path) -> Tuple[object, Path, Path]:
     """Extracts features from the uploaded image."""
     og_img_path = input_path
-    annotated_img_path = os.path.join(UPLOAD_DIR, "annotated_temp.jpg")
-    df_landmarks = extract_landmarks(input_path, annotated_img_path, is_video=False, write_output=True)
+    annotated_img_path = UPLOAD_DIR / "annotated_temp.jpg"
+    df_landmarks = extract_landmarks(str(input_path), str(annotated_img_path), is_video=False, write_output=True)
     
-    landmarks_csv_path = os.path.join(UPLOAD_DIR, "temp_landmarks.csv")
+    landmarks_csv_path = UPLOAD_DIR / "temp_landmarks.csv"
     df_landmarks.to_csv(landmarks_csv_path, index=False)
     
-    df_features = extract_features_from_single_landmark_csv(landmarks_csv_path, UPLOAD_DIR)
+    df_features = extract_features_from_single_landmark_csv(str(landmarks_csv_path), str(UPLOAD_DIR))
     return df_features, og_img_path, annotated_img_path
-
 
 def get_pose_prediction(df_features: object) -> Tuple[str, object, object]:
     """Predicts the pose and returns it along with probabilities and labels."""
@@ -69,7 +73,6 @@ def get_pose_prediction(df_features: object) -> Tuple[str, object, object]:
     pose_labels = label_encoder.classes_
     return pose_decoded, probabilities, pose_labels
 
-
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)) -> Dict[str, Union[str, float]]:
     """Handles file upload, feature extraction, pose prediction, and returns results."""
@@ -78,8 +81,8 @@ async def upload_file(file: UploadFile = File(...)) -> Dict[str, Union[str, floa
         df_features, og_img_path, annotated_img_path = extract_image_features(input_path)
         pose_decoded, probabilities, pose_labels = get_pose_prediction(df_features)
         
-        chart_filename = os.path.join(UPLOAD_DIR, "temp_chart.html")
-        create_probability_chart(probabilities, pose_labels, chart_filename, og_img_path)
+        chart_filename = UPLOAD_DIR / "temp_chart.html"
+        create_probability_chart(probabilities, pose_labels, str(chart_filename), str(og_img_path))
 
         current_timestamp = datetime.now().timestamp()
 
@@ -93,3 +96,4 @@ async def upload_file(file: UploadFile = File(...)) -> Dict[str, Union[str, floa
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
         raise HTTPException(status_code=500, detail=str(e))
+
